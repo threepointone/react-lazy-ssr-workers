af93390ef57c35366f1899b7282a17808e1a2c6a
Sunil Pai <spai@cloudflare.com>
2021-08-12 13:24:31 +0100

    try to fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 rm -rf out && mkdir -p out
 rm -rf static && mkdir -p static &&cp public/* static  
 STATIC_ROOT=https://react-lazy.coolcomputerclub.com
-npx concurrently "NODE_PATH=vendor esbuild worker/server.js --bundle --loader:.js=jsx --outfile=out/worker.mjs --format=esm --inject:./src/react-shim.js --define:STATIC_ROOT=\"${STATIC_ROOT}\"" "NODE_PATH=vendor esbuild src/index.js --bundle --loader:.js=jsx --outdir=static --format=esm --inject:./src/react-shim.js --splitting --public-path=${STATIC_ROOT} --define:STATIC_ROOT=\"${STATIC_ROOT}\"" -k
+npx concurrently "NODE_PATH=vendor esbuild worker/server.js --bundle --loader:.js=jsx --outfile=out/worker.mjs --format=esm --inject:./src/react-shim.js --define:STATIC_ROOT=\\\"${STATIC_ROOT}\\\"" "NODE_PATH=vendor esbuild src/index.js --bundle --loader:.js=jsx --outdir=static --format=esm --inject:./src/react-shim.js --splitting --public-path=${STATIC_ROOT} --define:STATIC_ROOT=\\\"${STATIC_ROOT}\\\"" -k
