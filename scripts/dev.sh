da0dce65dc23b7ba5a701790bed412a2d89cc1c5
Sunil Pai <spai@cloudflare.com>
2021-08-12 13:42:37 +0100

    fix build again

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,4 +1,4 @@
 rm -rf out && mkdir -p out
 rm -rf static && mkdir -p static &&cp public/* static  
 STATIC_ROOT=http://localhost:5000
-npx concurrently "NODE_PATH=vendor esbuild worker/server.js --bundle --loader:.js=jsx --outfile=out/worker.mjs --format=esm --watch --inject:./src/react-shim.js --define:STATIC_ROOT=\\\"${STATIC_ROOT}\\\"" "NODE_PATH=vendor esbuild src/index.js --bundle --loader:.js=jsx --outdir=static --format=esm --watch --inject:./src/react-shim.js --splitting --public-path=${STATIC_ROOT} --define:STATIC_ROOT=\\\"${STATIC_ROOT}\\\"" "serve static -C" "wrangler dev" -k
+npx concurrently "NODE_PATH=vendor esbuild worker/server.js --bundle --loader:.js=jsx --outfile=out/worker.mjs --format=esm --watch --inject:./src/react-shim.js --minify --define:STATIC_ROOT=\\\"${STATIC_ROOT}\\\"" "NODE_PATH=vendor esbuild src/index.js --bundle --loader:.js=jsx --outdir=static --format=esm --watch --inject:./src/react-shim.js --splitting --public-path=${STATIC_ROOT} --minify --define:STATIC_ROOT=\\\"${STATIC_ROOT}\\\"" "serve static -C" "wrangler dev" -k
